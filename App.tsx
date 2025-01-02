8ec6cb3dd4ea5cead559949fa7ed34011d74a3df
CreatorDevelopperPankaj <pankazbisht123@gmail.com>
2025-01-02 15:07:49 +0530

    add new text

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>this is the entry point our application </Text>
+      <Text>Hello word my name is kamal singh bisht</Text>
       <StatusBar style="auto" />
     </View>
   );
